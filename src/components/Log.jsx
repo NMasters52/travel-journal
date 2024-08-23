506a730ef6712a2732a1e8ce1c0dca442ee44eff
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-08-23 08:12:40 -0400

    layout of log info

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Log() {
     return (
-        <div>
-            <h1>log is working</h1>
-        </div>
+        <article>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <span> Location <a href="">Google Link</a></span>
+            <h3>Name of Course</h3>
+            <span>Dates Visited</span>
+            <p>Review/ experiances. Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nemo quaerat iusto, rerum ipsum unde labore qui laboriosam quos voluptatum eveniet minus ex assumenda animi obcaecati soluta asperiores alias adipisci, perferendis possimus? Repellendus esse reiciendis pariatur praesentium ea, qui architecto ad quibusdam. Illo facilis, autem eum fuga in culpa debitis.</p>
+        </article>
     )
 }
