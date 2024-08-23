a2257eb0d069335e39b0d4641e04e713291d54c6
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-08-23 05:57:40 -0400

    header.jsx and css setup and testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header.jsx'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
-   <div>
-    <h1>hello world</h1>
-   </div>
-  )
+    <div>
+      <Header />
+    </div>
+   )
 }
+  
+
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h1 className="header--title">My Disc Golf Travel Journal</h1>
+        </header>
+    )
+}
