0037741c6cc0622fe5d7a15e9bf2db0826c55575
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-08-23 06:13:02 -0400

    Header set up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <header className="header">
-            <h1 className="header--title">My Disc Golf Travel Journal</h1>
+            <h1><i class="fa-solid fa-mountain-city header--icon"></i>Masters Disc Golf Travel Journal</h1>
         </header>
     )
 }
