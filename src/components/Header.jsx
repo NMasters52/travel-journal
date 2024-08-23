232b6be61ad5b5b7e15204dd52c9bbbba475210c
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-08-23 08:04:57 -0400

    edit to title in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <header className="header">
-            <h1><i class="fa-solid fa-mountain-city header--icon"></i>Masters Disc Golf Travel Journal</h1>
+            <h1><i class="fa-solid fa-mountain-city header--icon"></i>Masters Disc Golf Travels</h1>
         </header>
     )
 }
