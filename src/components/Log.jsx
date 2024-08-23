015488f7e484a116a59227c40e1de851e1a519a6
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-08-23 08:37:36 -0400

    log post article layout set up

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -7,10 +7,14 @@ export default function Log() {
             <div className='log-post__img'>
                 <img className='log-post--img' src="../public/images/maple-hill.png" alt="Picture of Hole One Maple Hill" />
             </div>
-            <span>Leicester, MA  <a href="https://maps.app.goo.gl/FudnRcMm6UhVjEva8">View on Google Maps</a></span>
-            <h3>Maple Hill</h3>
-            <span>Dates Visited</span>
-            <p>Review/ experiances. Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nemo quaerat iusto, rerum ipsum unde labore qui laboriosam quos voluptatum eveniet minus ex assumenda animi obcaecati soluta asperiores alias adipisci, perferendis possimus? Repellendus esse reiciendis pariatur praesentium ea, qui architecto ad quibusdam. Illo facilis, autem eum fuga in culpa debitis.</p>
+            <div className='log-post__article'>
+                <span>Leicester, MA  <a href="https://maps.app.goo.gl/FudnRcMm6UhVjEva8">View on Google Maps</a>
+                </span>
+                <h3>Maple Hill</h3>
+                <span>Dates Visited</span>
+                <p>Review/ experiances. Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nemo quaerat iusto, rerum ipsum unde labore qui laboriosam quos voluptatum eveniet minus ex assumenda animi obcaecati soluta asperiores alias adipisci, perferendis possimus? Repellendus esse reiciendis pariatur praesentium ea, qui architecto ad quibusdam. Illo facilis, autem eum fuga in culpa debitis.</p>
+            </div>
+            
         </article>
     )
 }
