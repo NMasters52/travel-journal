a0bb68b2fa92d89fa9ec8bf5b367a1c196612980
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-08-24 06:09:45 -0400

    created a data file for props use

---
+++ src/data.js
@@ -0,0 +1,9 @@
+export default [
+    {
+       title: "Maple Hill Disc Golf",
+       location: " Leicester, MA",
+       googleMapsUrl: "https://maps.app.goo.gl/FudnRcMm6UhVjEva8",
+       description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+       imageUrl: "mapel-hill.png"
+   }
+]
