19b9d5a186ccac854499cc3f7aa78bc253237643
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-08-24 06:14:38 -0400

    setting up props mapping in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import Header from './components/Header.jsx'
 import Log from './components/Log.jsx'
 import './App.css'
+import data from './data.js'
 
 
 function App() {
+  const logData = data.map(log => {
+    return (
+      <Log 
+        key={index}
+        {...log}
+      />
+    )
+  }) 
   return (
       <>
         <Header />
